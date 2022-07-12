b435607da6c9704c53f8263bab8932a1d3f158ca
Victor Norman <vtn2@calvin.edu>
2022-07-12 12:41:03 -0400

    fix tests

--- testPythonLikeList.cpp
+++ testPythonLikeList.cpp
@@ -1,106 +1,78 @@
-#include <cassert>
+#define CATCH_CONFIG_MAIN
 #include <iostream>
 
 #include "PythonLikeList.h"
+#include "catch.hpp"
 using namespace std;
 
-
-int main() {
-    cout << "Test constructors...\n";
+TEST_CASE("constructors") {
     PythonLikeList lst;
-    assert(lst.getSize() == 0);
-
     PythonLikeList lst2(5);
-    assert(lst2.getSize() == 5);
-
-    cout << "Testing getValue\n";
-    assert(lst2.getValue(0) == "");
-    assert(lst2.getValue(4) == "");
-    try {
-        assert(lst2.getValue(5) == "");
-        assert(false);
-    } catch (const range_error &re) {
-        // do nothing here: All is well!
+    SECTION("empty") { REQUIRE(lst.getSize() == 0); }
+    SECTION("nonempty") { REQUIRE(lst2.getSize() == 5); }
+    SECTION("getValue") {
+        REQUIRE(lst2.getValue(0) == "");
+        REQUIRE(lst2.getValue(4) == "");
+        REQUIRE_THROWS_AS(lst2.getValue(5), range_error);
     }
-
-    cout << "testing setValue\n";
-    lst2.setValue(0, "hi");
-    assert(lst2.getValue(0) == "hi");
-    try {
-        assert(lst2.getValue(5) == "");
-        assert(false);
-    } catch (const range_error &re) {
-        // do nothing here: All is well!
+    SECTION("setValue") {
+        lst2.setValue(0, "hi");
+        REQUIRE(lst2.getValue(0) == "hi");
+        REQUIRE_THROWS_AS(lst2.getValue(5), range_error);
     }
-
-    cout << "Testing []\n";
-    assert(lst2[0] == "hi");  // getting the value
-    lst2[1] = "hello";        // setting the value.
-    assert(lst2[1] == "hello");
-    try {
-        assert(lst2[5] == "");
-        assert(false);
-    } catch (const range_error &re) {
-        // do nothing here: All is well!
+    SECTION("subscripting") {
+        REQUIRE(lst2[0] == "hi");  // getting the value
+        lst2[1] = "hello";         // setting the value.
+        REQUIRE(lst2[1] == "hello");
+        REQUIRE_THROWS_AS(lst2[5], range_error);
     }
-
-    cout << "Test copy constructor\n";
-    PythonLikeList lst3(lst2);
-    assert(lst3.getSize() == lst2.getSize());
-    lst2[2] = "please work!";
-    assert(lst3[2] != "please work!");
-
-    cout << "Testing printing...\n";
-    PythonLikeList lst4(2);
-    lst4[0] = "Emma";
-    lst4[1] = "Wang";
-    cout << "This is a list -->" << lst4 << "<--\n";
-
-    cout << "Testing append...\n";
-    PythonLikeList aList;
-    assert(aList.getSize() == 0);
-    aList.append("Alex");
-    assert(aList.getSize() == 1);
-    assert(aList[0] == "Alex");
-    cout << aList << endl;
-
-    aList.append("Heyab");
-    assert(aList.getSize() == 2);
-    assert(aList[0] == "Alex");
-    assert(aList[1] == "Heyab");
-    cout << aList << endl;
-
-    aList.append("Prof Norman is weird.");
-    cout << aList << endl;
-
-    cout << "Testing operator+" << endl;
-    PythonLikeList l1;
-    l1.append("Hi");
-    PythonLikeList l2;
-    l2.append("Nihao");
-    l2.append("Konnichiwa");
-    PythonLikeList l3 = l1 + l2;
-    cout << l3 << endl;
-    assert(l3.getSize() == 3);
-    assert(l3[0] == "Hi");
-    assert(l3[1] == "Nihao");
-    assert(l3[2] == "Konnichiwa");
-
-    // cout << "Testing negative indexes..." << endl;
-    // assert(l3[-1] == "Konnichiwa");
-    //assert(l3[-2] == "Nihao");
-    // assert(l3.getItem(-2) == "Nihao");
-    // try {
- //	l3[-4];
- //	assert(false);
-    // } catch (const range_error &re) {
-     //	// nothing here.
+    SECTION("copy constructor") {
+        PythonLikeList lst3(lst2);
+        REQUIRE(lst3.getSize() == lst2.getSize());
+        lst2[2] = "please work!";
+        REQUIRE(lst3[2] != "please work!");
+    }
+    SECTION("printing") {
+        PythonLikeList lst4(2);
+        lst4[0] = "Victor";
+        lst4[1] = "Norman";
+        std::cout << "This is a list -->" << lst4 << "<--\n";
+    }
+    SECTION("append") {
+        PythonLikeList aList;
+        REQUIRE(aList.getSize() == 0);
+        aList.append("Alex");
+        REQUIRE(aList.getSize() == 1);
+        REQUIRE(aList[0] == "Alex");
+        aList.append("Betty");
+        REQUIRE(aList.getSize() == 2);
+        REQUIRE(aList[0] == "Alex");
+        REQUIRE(aList[1] == "Betty");
+    }
+    SECTION("operator+") {
+        PythonLikeList l1;
+        l1.append("Hi");
+        PythonLikeList l2;
+        l2.append("Nihao");
+        l2.append("Konnichiwa");
+        PythonLikeList l3 = l1 + l2;
+        REQUIRE(l3.getSize() == 3);
+        REQUIRE(l3[0] == "Hi");
+        REQUIRE(l3[1] == "Nihao");
+        REQUIRE(l3[2] == "Konnichiwa");
+    }
+    // SECTION("negative indexes") {
+    // PythonLikeList l4;
+    // l4.append("Hi");
+    // l4.append("Nihao");
+    // l4.append("Konnichiwa");
+    // REQUIRE(l4[-1] == "Konnichiwa");
+    // REQUIRE(l4[-2] == "Nihao");
+    // REQUIRE(l4.getItem(-2) == "Nihao");
+    // REQUIRE_THROWS_AS(l4[-4], range_error);
+    // l4[-1] = "AufWiedersehen";
+    // REQUIRE(l4[2] == "AufWiedersehen");
+    // l4.setItem(-1, "Hallo");
+    // REQUIRE(l4[2] == "Hallo");
     // }
-    // l3[-1] = "AufWiedersehen";
-    // assert(l3[2] == "AufWiedersehen");
-    // l3.setItem(-1, "Hallo");
-    // assert(l3[2] == "Hallo");
-
-    cout << "All tests passed!\n";
-    return 0;
 }
